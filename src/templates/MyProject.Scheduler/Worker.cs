430a578d7aed89c0e62986d717d5e8209f467933
Alexanderius <alexander.krylkov@gmail.com>
2023-09-09 22:45:59 +0600

    [edit] output to console

--- src/templates/MyProject.Scheduler/Worker.cs
+++ src/templates/MyProject.Scheduler/Worker.cs
@@ -1,8 +1,9 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 
 namespace MyProject.Scheduler;
 
 internal class Worker
 {
-	public void Run() => Trace.WriteLine("Hello world!");
+	public void Run() => Console.WriteLine("Hello world!");
 }
